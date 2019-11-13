c3b4df077ffada02645543841ca0600466dbf017
GD7 <GD7@DESKTOP-FPB01NN>
2019-11-13 16:22:22 +0900

    11/13

--- src/변수.java
+++ src/변수.java
@@ -4,9 +4,17 @@ public class 변수 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("변수");
-		afsdasfadas
+		short 년도=2000;
+		short 윤년=0;
+		while(년도<=2019 && 년도>=2000) {
+			if(년도%4==0 && 년도%100!=0) {
+				윤년++;
+			} else if (년도%400==0) {
+				윤년++;
+			}
+			년도++;
+		}
+		System.out.println(윤년);
+		
 	}
-	
-
 }
